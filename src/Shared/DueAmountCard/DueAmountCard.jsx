d344b11338bcb08933709abc5a4a6915ddd41ee6
Rishabh Varshney <right.rishabh@gmail.com>
2023-11-10 21:33:49 +0530

    Update DueAmountCard.css with table head styles

--- src/Shared/DueAmountCard/DueAmountCard.jsx
+++ src/Shared/DueAmountCard/DueAmountCard.jsx
@@ -4,77 +4,91 @@ const DueAmountCard = ({ title, due_amount, currency_abv, linked_item1, linked_i
     return (
         <div className="dueAmount__container">
             <div className="dueAmount__main">
-                <h3 className="dueAmount__main--head">Balance Amount</h3>
+                {
+                    title === "Tax Invoice" ?
+                        <h3 className="dueAmount__main--head">Amount Due</h3>
+                        : <h3 className="dueAmount__main--head">Balance Amount</h3>
+                }
                 <h2 className="dueAmount__main--amount">{currency_abv} {due_amount}</h2>
                 {
                     title === "Tax Invoice" ?
                         <>
-                            <div className="dueAmount--table">
-                                <div className="dueAmount--table-head">
-                                    <div>Receipt Id</div>
-                                    <div>Receipt Number</div>
-                                    <div>Amount</div>
-                                </div>
-                                <div className="dueAmount--table-body">
-                                    {
-                                        linked_item1?.length > 0 ? linked_item1?.map((receipt, index) => {
-                                            return (
-                                                <div key={index} className="dueAmount--table--data">
-                                                    <div>{receipt.receipt_id}</div>
-                                                    <div>{receipt.receipt_number}</div>
-                                                    <div>{receipt.amount}</div>
-                                                </div>
-                                            )
-                                        }) :
-                                            <span className="dueAmount--table-noData">No Receipt</span>
-                                    }
-                                </div>
-                            </div>
-                            <div className="dueAmount--table">
-                                <div className="dueAmount--table-head">
-                                    <div>Credit Note Id</div>
-                                    <div>Credit Note Number</div>
-                                    <div>Amount</div>
-                                </div>
-                                <div className="dueAmount--table-body">
-                                    {
-                                        linked_item2?.length > 0 ? linked_item2?.map((cn, index) => {
-                                            return (
-                                                <div key={index}>
-                                                    <div>{cn.cn_id}</div>
-                                                    <div>{cn.cn_number}</div>
-                                                    <div>{cn.amount}</div>
-                                                </div>
-                                            )
-                                        }) :
-                                            <span className="dueAmount--table-noData">No Credit Notes</span>
-                                    }
-                                </div>
-                            </div>
+                            {
+                                linked_item1?.length > 0 ?
+                                    <div className="dueAmount--table">
+                                        <div className="dueAmount--table-head">
+                                            <div className="dueAmount--table-head1">Receipt Id</div>
+                                            <div className="dueAmount--table-head2">Receipt Number</div>
+                                            <div className="dueAmount--table-head3">Amount</div>
+                                        </div>
+                                        <div className="dueAmount--table-body">
+                                            {
+                                                linked_item1?.length > 0 ? linked_item1?.map((receipt, index) => {
+                                                    return (
+                                                        <div key={index} className="dueAmount--table--data">
+                                                            <div className="dueAmount--table-head1">{receipt.receipt_id}</div>
+                                                            <div className="dueAmount--table-head2">{receipt.receipt_number}</div>
+                                                            <div className="dueAmount--table-head3">{receipt.amount}</div>
+                                                        </div>
+                                                    )
+                                                }) :
+                                                    <span className="dueAmount--table-noData">No Receipt</span>
+                                            }
+                                        </div>
+                                    </div> : ""
+                            }
+                            {
+                                linked_item2?.length > 0 ?
+                                    <div className="dueAmount--table">
+                                        <div className="dueAmount--table-head">
+                                            <div className="dueAmount--table-head1">Credit Note Id</div>
+                                            <div className="dueAmount--table-head2">Credit Note Number</div>
+                                            <div className="dueAmount--table-head3">Amount</div>
+                                        </div>
+                                        <div className="dueAmount--table-body">
+                                            {
+                                                linked_item2?.length > 0 ? linked_item2?.map((cn, index) => {
+                                                    return (
+                                                        <div key={index} className="dueAmount--table--data">
+                                                            <div className="dueAmount--table-head1">{cn.cn_id}</div>
+                                                            <div className="dueAmount--table-head2">{cn.cn_number}</div>
+                                                            <div className="dueAmount--table-head3">{cn.amount}</div>
+                                                        </div>
+                                                    )
+                                                }) :
+                                                    <span className="dueAmount--table-noData">No Credit Notes</span>
+                                            }
+                                        </div>
+                                    </div> : ""
+                            }
                         </>
                         :
                         <>
-                            <div className="dueAmount--table">
-                                <div className="dueAmount--table-head">
-                                    <div>Invoice Id</div>
-                                    <div>Invoice Number</div>
-                                    <div>Amount</div>
-                                </div>
-                                <div className="dueAmount--table-body">
-                                    {
-                                        linked_item1?.length > 0 ? linked_item1?.map((invoice, index) => {
-                                            return (
-                                                <div key={index} className="dueAmount--table--data">
-                                                    <div>{invoice.invoice_id}</div>
-                                                    <div>{invoice.invoice_number}</div>
-                                                    <div>{invoice.amount}</div>
-                                                </div>
-                                            )
-                                        }) :
-                                            <span className="dueAmount--table-noData">No Receipt</span>
-                                    }
-                                </div>
-                            </div>
+                            {
+                                linked_item1?.length > 0 ?
+                                    <div className="dueAmount--table">
+                                        <div className="dueAmount--table-head">
+                                            <div className="dueAmount--table-head1">Invoice Id</div>
+                                            <div className="dueAmount--table-head2">Invoice Number</div>
+                                            <div className="dueAmount--table-head3">Amount</div>
+                                        </div>
+                                        <div className="dueAmount--table-body">
+                                            {
+                                                linked_item1?.length > 0 ? linked_item1?.map((invoice, index) => {
+                                                    return (
+                                                        <div key={index} className="dueAmount--table--data">
+                                                            <div className="dueAmount--table-head1">{invoice.invoice_id}</div>
+                                                            <div className="dueAmount--table-head2">{invoice.invoice_number}</div>
+                                                            <div className="dueAmount--table-head3">{invoice.amount}</div>
+                                                        </div>
+                                                    )
+                                                }) :
+                                                    <span className="dueAmount--table-noData">No Receipt</span>
+                                            }
+                                        </div>
+                                    </div>
+                                    : ""
+                            }
                         </>
                 }
             </div>
